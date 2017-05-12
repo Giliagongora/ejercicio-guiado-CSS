73f7647e72f7a5153c0036f3281db74358c0738d
Gilia Gongora <giliagongora.g@gmail.com>
2017-05-11 22:46:16 -0300

    ultimos cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
+var lastScrollTop = 0;
+window.addEventListener("scroll", function(){
+	var currentScroll = window.pageYOffset || document.body.scrolltop;
+	if (currentScroll>lastScrollTop){
+		console.log("down"); //down
+	}else{
+		console.log("up"); // up
+	}
+	lastScrollTop = currentScroll; // ignoren esto por ahora
+});
+
 document.getElementById("toogle").addEventListener("click", function(event){
 	event.preventDefault();
 	document.getElementById("nav-header").classList.toogle("open");
